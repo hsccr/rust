e490c87f6e19bc1c4719241cacbc0c7c36912d61
Chungrae Cho <ccr@emstone.com>
2021-01-08 14:01:08 +0900

    httpd: usage state

--- nems/src/httpd.rs
+++ nems/src/httpd.rs
@@ -1,8 +1,19 @@
 use rocket::{get, put, post, delete, head, patch, options, routes};
-use rocket_contrib::json::Json;
 use rocket::http::Status;
 use rocket::response::status::Custom;
 use serde::{Deserialize, Serialize};
+
+use std::collections::HashMap;
+
+use rocket::State;
+use rocket_contrib::json::Json;
+use rocket_contrib::uuid::Uuid;
+use rocket_contrib::uuid::uuid_crate as uuid;
+
+// A small people mapping in managed state for the sake of this example. In a
+// real application this would be a database. Notice that we use the uuid::Uuid
+// type here and not the rocket_contrib::uuid::Uuid type.
+struct People(HashMap<uuid::Uuid, &'static str>);
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
 struct PostData {
@@ -98,6 +109,15 @@ fn httpd_options() -> Json<&'static str> {
   }")
 }
 
+#[get("/people/<id>")]
+fn people(id: Uuid, people: State<People>) -> Result<String, String> {
+    // Because Uuid implements the Deref trait, we use Deref coercion to convert
+    // rocket_contrib::uuid::Uuid to uuid::Uuid.
+    Ok(people.0.get(&id)
+        .map(|person| format!("We found: {}", person))
+        .ok_or_else(|| format!("Person not found for UUID: {}", id))?)
+}
+
 pub fn start()
 {
   println!("start ...");
@@ -112,10 +132,17 @@ pub fn start()
     patch - PATCH specific route
   */
 
+  let mut map = HashMap::new();
+  map.insert("7f205202-7ba1-4c39-b2fc-3e630722bf9f".parse().unwrap(), "Lacy");
+  map.insert("4da34121-bc7d-4fc1-aee6-bf8de0795333".parse().unwrap(), "Bob");
+  map.insert("ad962969-4e3d-4de7-ac4a-2d86d6d10839".parse().unwrap(), "George");
+
   rocket::ignite()
-  .mount(
+        .manage(People(map))
+        .mount(
       "/",
       routes![
+          people,
           hello,
           hi,
           httpd_head,
